e0d8cbb91201c352dac41142e0f1c00f72c568c2
Clare <1090535708@qq.com>
2025-03-27 20:30:19 +0800

    modify filse: utf8 assert

--- include/spdlog/fmt/bundled/base.h
+++ include/spdlog/fmt/bundled/base.h
@@ -459,8 +459,10 @@ enum { use_utf8 = !FMT_WIN32 || is_utf8_enabled };
 #  define FMT_UNICODE 1
 #endif
 
-static_assert(!FMT_UNICODE || use_utf8,
+//TODO(clare): fail to compile as though it's true
+static_assert(!FMT_UNICODE || !use_utf8,
               "Unicode support requires compiling with /utf-8");
+
 
 template <typename T> constexpr const char* narrow(const T*) { return nullptr; }
 constexpr FMT_ALWAYS_INLINE const char* narrow(const char* s) { return s; }
